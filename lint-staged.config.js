e4bfaf95c48e7d847e79c4a8f7b91673b7b6891e
puromed <boymanpunchman12@gmail.com>
2025-10-27 15:50:40 +0800

    git commit -m "Initial setup with linting, prettier, mail, and database"

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,9 @@
+
 export default {
-  '**/*.php': ['./vendor/bin/sail bin duster fix'],
-  '**/*': ['./vendor/bin/sail npx prettier --write --ignore-unknown'],
+  '*.php': [
+    './vendor/bin/sail bin duster fix',
+  ],
+  '*': [
+    './vendor/bin/sail npx prettier --write --ignore-unknown',
+  ],
 };
