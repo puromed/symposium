3806d4e78dd53775a9283265d01b1d8565ae7809
puromed <boymanpunchman12@gmail.com>
2025-10-28 16:04:28 +0800

    Created migrations

---
+++ database/factories/ConferenceFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Conference>
+ */
+class ConferenceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $startsAt = now()->addMonths(6);
+        $endsAt = $startsAt->clone()->addDays(3);
+        $cfp_starts_at = $startsAt->clone()->subMonths(4);
+        $cfp_ends_at = $startsAt->clone()->addMonths(2);
+
+        return [
+            'title' => fake()->sentence(),
+            'location' => fake()->city() . ', ' . fake()->country(),
+            'description' => fake()->paragraph(),
+            'url' => fake()->url(),
+            'starts_at' => $startsAt,
+            'ends_at' => $endsAt,
+            'cfp_starts_at' => $cfp_starts_at,
+            'cfp_ends_at' => $cfp_ends_at,
+           
+        ];
+    }
+}
